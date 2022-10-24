52a4051c28c1f16c61012851eb6e0ea3b883e2fc
nttnhannho <makelifeperfect@gmail.com>
2022-10-24 19:05:14 +0700

    Authentication in FastAPI

--- backend/core/security.py
+++ backend/core/security.py
@@ -1,8 +1,8 @@
-from datetime import timedelta, datetime
+from datetime import datetime
+from datetime import timedelta
 from typing import Optional
 
 from core.config import settings
-
 from jose import jwt
 
 
@@ -11,8 +11,12 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.utcnow() + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
 
     return encoded_jwt
